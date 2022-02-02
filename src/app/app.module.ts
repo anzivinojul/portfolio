7051f0071eac53f165c91c8f35c5a947c5549f0b
anzivinojul <anzivinojulien@gmail.com>
2022-02-02 16:43:15 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { CvEducationComponent } from './pages/cv-education/cv-education.component';
+import { HeaderComponent } from './templates/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    CvEducationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
