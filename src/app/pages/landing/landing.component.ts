e440a0d0d0d60dc772870b023a8d574f4ec083b8
anzivinojul <anzivinojulien@gmail.com>
2022-02-08 20:47:21 +0100

    add links to buttons on landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -9,6 +9,32 @@ export class LandingComponent implements OnInit {
 
   constructor() { }
 
+  goCV(): void {
+    const cv_education = document.querySelector('.container-cv-education');
+    const height = (cv_education.getBoundingClientRect().top + window.scrollY) - 128;
+
+    window.scrollTo(
+      {
+        top: height,
+        left: 0,
+        behavior: 'smooth'
+      }
+    );
+  }
+
+  goProject(): void {
+    const project = document.querySelector('.container-projects');
+    const height = (project.getBoundingClientRect().top + window.scrollY) - 128;
+
+    window.scrollTo(
+      {
+        top: height,
+        left: 0,
+        behavior: 'smooth'
+      }
+    );
+  }
+
   ngOnInit(): void {
   }
 
